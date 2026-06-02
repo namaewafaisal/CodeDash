89e5638fe26875ea2fb655c30b0017a98f8fbd13
namaewafaisal <mohamed.faisal.4626@gmail.com>
2026-06-02 16:53:31 +0530

    Feature: Sync debug controller

--- src/main/java/com/codedash/fetch/DebugController.java
+++ src/main/java/com/codedash/fetch/DebugController.java
@@ -1,5 +1,6 @@
 package com.codedash.fetch;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class DebugController {
     private final FetchSchedulerService fetchSchedulerService;
 
     @PostMapping("/sync")
+    @PreAuthorize("hasRole('MASTER')")
     public String sync() {
 
         fetchSchedulerService.syncLeetcodeHandles();
